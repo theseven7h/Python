fd04e36b0956372a0b614fbc4b025fa9a024d855
theseven7h <jamestauri123@gmail.com>
2025-05-03 00:55:22 +0100

    Completed odd or even number check

---
+++ Chapter 2/odd_or_even.py
@@ -0,0 +1,4 @@
+"""Determining odd and even numbers"""
+number = int(input("Enter a number: "))
+if (number % 2 == 0): print(number,"is an even number")
+if (number % 2 != 0): print(number,"is an odd number")
