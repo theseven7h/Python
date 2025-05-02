a5b0373f9f1d16f047ccd3ec7aeb26e42796e020
theseven7h <jamestauri123@gmail.com>
2025-05-03 00:54:21 +0100

    Completed

---
+++ Chapter 2/multiples.py
@@ -0,0 +1,5 @@
+"""Multiples of 1024 and 2"""
+if (1024 % 4 == 0): print("1024 is a multiple of 4")
+if (1024 % 4 != 0): print("1024 is not multiple of 4")
+if (2 % 10 == 0): print("2 is a multiple of 10")
+if (2 % 4 == 0): print("2 is a multiple of 10")
