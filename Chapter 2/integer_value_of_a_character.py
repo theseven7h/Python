ad0eeed45e1a8dea0e61270f8b3717f8f4212c6b
theseven7h <jamestauri123@gmail.com>
2025-05-03 01:33:16 +0100

    Characters completed

---
+++ Chapter 2/integer_value_of_a_character.py
@@ -0,0 +1,20 @@
+"""Integer value of a character"""
+print(f"""
+B: {ord('B')}
+C: {ord('C')}
+D: {ord('D')}
+b: {ord('b')}
+c: {ord('c')}
+d: {ord('d')}
+0: {ord('0')}
+1: {ord('1')}
+2: {ord('2')}
+$: {ord('$')}
+*: {ord('*')}
++: {ord('+')}
+ : {ord(' ')}
+""")
+
+
+
+
