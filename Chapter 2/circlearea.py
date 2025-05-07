d7c93bca488bf4350f839b5b260998e0bd14c93f
theseven7h <jamestauri123@gmail.com>
2025-05-07 08:30:36 +0100

    modification

--- Chapter 2/circlearea.py
+++
@@ -1,7 +0,0 @@
-"""Callculating diameter, circumference and area of a circle with radius 2"""
-pi = 3.14159
-radius = 2
-diameter = 2 * radius
-circumference = 2 * pi * radius
-area = pi * (radius ** 2)
-print(f"Diameter: {diameter}\nCircumference: {circumference:.2f}\nArea: {area:.2f}")
--- Chapter 2/investment_return.py
+++ Chapter 2/investment_return.py
@@ -1,7 +1,7 @@
 """7% Investment Return"""
 principal = float(1000)
 annual_rate = float(7)
-number_of_years = 10;
+
 year_ten_amount = principal * ((1 + (annual_rate / 100)) ** 10)
 year_twenty_amount = principal * ((1 + (annual_rate / 100)) ** 20)
 year_thirty_amount = principal * ((1 + (annual_rate / 100)) ** 30)
@@ -11,4 +11,5 @@ Amount at end of 10 years is %.2f
 Amount at end of 20 years is %.2f
 Amount at end of 30 years is %.2f
 """ % (year_ten_amount, year_twenty_amount, year_thirty_amount))
+
 
