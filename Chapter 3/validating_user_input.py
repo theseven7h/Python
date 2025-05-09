c5e7c195005981d5c65313e48e8bbcc7025107fb
theseven7h <jamestauri123@gmail.com>
2025-05-09 06:56:09 +0100

    Fig 3.3 modification done

---
+++ Chapter 3/validating_user_input.py
@@ -0,0 +1,17 @@
+passes = 0
+failures = 0
+
+for student in range(10):
+	result = int(input("Enter result (1=pass, 2=fail): "))
+	while result != 1 and result != 2:
+		result = int(input("Invalid input...\nEnter result (1=pass, 2=fail): "))
+	if result == 1:
+		passes = passes + 1
+	else:
+		failures = failures + 1
+		
+print('Passed:', passes)
+print('Failed:', failures)
+
+if passes > 8:
+	print("Bonus to instructor")
