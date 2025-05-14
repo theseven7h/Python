f045e07d74a4b1a0861d9bfa40071c046e7daf0d
theseven7h <jamestauri123@gmail.com>
2025-05-14 10:10:39 +0100

    Pythagorean theorem modified

--- Chapter 3/pythagorean_triples.py
+++ Chapter 3/pythagorean_triples.py
@@ -1,7 +1,7 @@
-for i in range(1, 21):
-	for j in range(i, 21):
-		for k in range(j, 21):
-			if i ** 2 + j ** 2 == k ** 2:
-				print(f"({i}, {j}, {k})")
+for side1 in range(1, 21):
+	for side2 in range(side1, 21):
+		for hypotenuse in range(side2, 21):
+			if side1 ** 2 + side2 ** 2 == hypotenuse ** 2:
+				print(f"({side1}, {side2}, {hypotenuse})")
 			
 	
