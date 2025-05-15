cbfabae1bfd533ed1d6b6b8ccd880f78c886cdb8
theseven7h <jamestauri123@gmail.com>
2025-05-15 09:27:29 +0100

    Two numbers comparator done

---
+++ Create/comparator.py
@@ -0,0 +1,8 @@
+first_number = input("Enter first number: ")
+second_number = input("Enter second number: ")
+if first_number == second_number:
+	print("0")
+elif first_number > second_number:
+	print("1")
+else:
+	print("-1")
