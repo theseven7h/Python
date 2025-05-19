303b162c5d89d4fc870093cf3c49349bd42c7dd0
theseven7h <jamestauri123@gmail.com>
2025-05-19 05:15:55 +0100

    5 tests done and passed

--- Tasks/testcategorizenumbersfunction.py
+++ Tasks/testcategorizenumbersfunction.py
@@ -10,7 +10,19 @@ class TestCategorizeNumbers(unittest.TestCase):
 		divisor = 5
 		categorizenumbers.categorize_numbers(numbers, divisor)
 		
-	def test_list_function_returns_results(self):
+	def test_function_returns_result_when_divisible_number_found(self):
 		actual = categorizenumbers.categorize_numbers([5, 7, 20, 33, 35], 5)
-		expected = (5, 20, 35)
+		expected = [5, 20, 35]
 		self.assertEqual(actual, expected)
+		
+	def test_function_prints_message_if_no_divisor_found(self):
+		actual = categorizenumbers.categorize_numbers([5, 7, 20, 33, 35], 9)
+		expected = "No divisible number found"
+		self.assertEqual(actual, expected)
+		
+	def test_if_divisor_is_0(self):
+		#actual = categorizenumbers.categorize_numbers([5, 7, 20, 33, 35], 0)
+		expected = "Divisor cannot be zero"
+		self.assertEqual(expected, categorizenumbers.categorize_numbers([5, 7, 20, 33, 35], 0))
+		
+	
