9369ddd5ceec6c431916d391c6bda22227897e60
theseven7h <jamestauri123@gmail.com>
2025-05-23 00:58:37 +0100

    Function used to print table of conversion from 0 - 100

--- Chapter 4/temperatureconversion.py
+++ Chapter 4/temperatureconversion.py
@@ -1,3 +1,6 @@
 def temperature_conversion(celcius):
 	return (9 / 5) * celcius + 32
-	
+
+print(f"{'Celcius':<10}{'Fahrenheit'}")	
+for celcius in range(0, 101):
+	print(f"{celcius:>7}{temperature_conversion(celcius):12.1f}")
