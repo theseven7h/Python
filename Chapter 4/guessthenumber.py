208ae5b66d40887ed5d2509ca3ea767fbdc004a3
theseven7h <jamestauri123@gmail.com>
2025-05-23 02:13:39 +0100

    Guess number game remodified

--- Chapter 4/guessthenumber.py
+++ Chapter 4/guessthenumber.py
@@ -1,18 +1,25 @@
 from random import randrange
 
 mystery_number = randrange(1, 1001, 1)
-
+print("Guess my number between 1 and 1000 with the fewest guesses")
+count = 1
 while True:
-	user_number = int(input("\nGuess my number between 1 and 1000 with the fewest guesses: "))
+	user_number = int(input(f"Attempt {count}: "))
 	if user_number == mystery_number:
 		print("Congratulations. You guessed the number!") 
 		print()
-		replay = int(input("Play again(1) or quit(0)?: "))
 		mystery_number = randrange(1, 1001, 1)
-		if replay == 0:
-			print("Thank you for playing ^_^...")
-			break
+		while True:	
+			replay = int(input("Play again(1) or quit(0)?: "))
+			if replay == 0:
+				print("Thank you for playing ^_^...")
+				break
+			else:
+				print("Wrong input...")
+		break
 	elif user_number > mystery_number:
 		print("Too high. Try again.")
 	elif user_number < mystery_number:
 		print("Too low. Try again.")
+	print()
+	count += 1
