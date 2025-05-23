34d31a8ed10a1c7723fece9ea23ccf29d7de9ad4
theseven7h <jamestauri123@gmail.com>
2025-05-23 03:15:27 +0100

    Guess number game function remodified

--- Chapter 4/guessthenumber.py
+++ Chapter 4/guessthenumber.py
@@ -1,25 +1,29 @@
 from random import randrange
 
-mystery_number = randrange(1, 1001, 1)
-print("Guess my number between 1 and 1000 with the fewest guesses")
-count = 1
-while True:
-	user_number = int(input(f"Attempt {count}: "))
-	if user_number == mystery_number:
-		print("Congratulations. You guessed the number!") 
+def mystery_number():
+	mystery_number = randrange(1, 1001, 1)
+	print(mystery_number,"Guess my number between 1 and 1000 with the fewest guesses")
+	count = 1
+	while True:
+		user_number = int(input(f"Attempt {count}: "))
+		if user_number == mystery_number:
+			print("\nCongratulations. You guessed the number!") 
+			return
+		elif user_number > mystery_number:
+			print("Too high. Try again.")
+		elif user_number < mystery_number:
+			print("Too low. Try again.")
 		print()
-		mystery_number = randrange(1, 1001, 1)
-		while True:	
-			replay = int(input("Play again(1) or quit(0)?: "))
-			if replay == 0:
-				print("Thank you for playing ^_^...")
-				break
-			else:
-				print("Wrong input...")
-		break
-	elif user_number > mystery_number:
-		print("Too high. Try again.")
-	elif user_number < mystery_number:
-		print("Too low. Try again.")
+		count += 1
+				
+while True:	
+	mystery_number()
+	replay = int(input("\nPlay again(1) or quit(0)?: "))
 	print()
-	count += 1
+	if replay == 0:
+		print("Thank you for playing ^_^...")
+		break
+	elif replay == 1:
+		continue
+	else:
+		print("Wrong input...")
