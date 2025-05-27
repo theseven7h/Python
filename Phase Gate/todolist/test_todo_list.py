d9e1f7bd83090faeb5b7b63a0babc97578a20d93
theseven7h <jamestauri123@gmail.com>
2025-05-27 12:09:13 +0100

    todo list view_tasks function in progress with test

--- Phase Gate/todolist/test_todo_list.py
+++ Phase Gate/todolist/test_todo_list.py
@@ -3,9 +3,16 @@ import todo_list
 from unittest import TestCase
 
 class TestTodoList(TestCase):
-	def test_add_task_function_exists(self):
-		todo_list.add_task()
+	'''def test_add_task_function_exists(self):
+		tasks = []
+		todo_list.add_task(tasks)
 		
 	def test_add_task_function_returns_right_task(self):
-		tasks = todo_list.add_task()
-		self.assertEqual(1, len(tasks))
+		tasks = []
+		added_tasks = todo_list.add_task(tasks)
+		self.assertEqual(1, len(tasks))'''
+		
+	def test_view_tasks_function_exists(self):
+		tasks = []
+		todo_list.view_tasks([])
+		
--- Phase Gate/todolist/todo_list.py
+++ Phase Gate/todolist/todo_list.py
@@ -1,10 +1,38 @@
 
-
-def add_task():
-	tasks = []
+def add_task(tasks):
 	task = input("Enter the task: ").strip()
 	tasks.append(task)
+	print("Task added!")
 	return tasks
 
+def view_tasks(tasks):
+	if len(tasks) == 0:
+		return "There are no tasks to view"
+	else:
+		count = 1
+		for task in tasks:
+			print(f"Task {count}: {task}")
+			count += 1
 
-#add_task()	
+
+def main():
+	tasks = []
+	while True:
+		main_menu = input("""
+----TO-DO LIST MANAGER----
+1. Add a task
+2. View tasks
+3. Mark task as complete
+4. Delete a task
+5. Exit
+		""").strip()
+		
+		match main_menu:
+			case "1": add_task(tasks)
+			case "2": view_tasks(tasks)
+			case "5": break
+			case _: print("Invalid input")
+			
+	
+main()
+	
