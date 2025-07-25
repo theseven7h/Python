72d60a218374038af4a0cbcf890d20a05dd1a529
theseven7h <jamestauri123@gmail.com>
2025-07-25 16:38:41 +0100

    regex task modified

--- Tasks/regex_tasks.py
+++ Tasks/regex_tasks.py
@@ -5,8 +5,9 @@ output_1 = re.findall(r'\d{3}-\d{3}-\d{4}', text_1)
 print(output_1)
 # print(re.findall(r'[0-9-]+', text_1))
 
-text_2 = 'tolu@yahoomail.com'
-output_2 = re.findall('[A-Za-z]+[0-9]*@[a-z]+mail.com', text_2)
+text_2 = 'tolu@gmail.com'
+# output_2 = re.findall(r'[a-z]+[0-9]*@gmail\.com|[a-z]+[0-9]*@yahoo\.com', text_2)
+output_2 = re.findall(r'[a-z]+[0-9]*@(?:gmail|yahoo)\.com', text_2)
 print(output_2)
 
 text_3 = 'Alice and Bob are Good Friends'
